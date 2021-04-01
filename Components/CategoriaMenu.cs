dcca4b7684483995b0da2f9252a070d0a0a8cda6
FranzFialho <franzfialho@gmail.com>
2021-04-01 03:43:58 -0300

    Criando um ViewComponent para exibir novos itens de menu

---
+++ Components/CategoriaMenu.cs
@@ -0,0 +1,27 @@
+﻿using LanchesFF.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesFF.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+
+            return View(categorias);
+        }
+
+    }
+}
